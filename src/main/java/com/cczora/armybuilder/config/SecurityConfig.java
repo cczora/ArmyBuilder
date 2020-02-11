9fc2a3e6613f393d6e1285901c44683c9b021206
cczora <czora@gmail.com>
2020-02-11 11:49:59 -0600

    added @Lazy to fix circular injection issues

--- src/main/java/com/cczora/armybuilder/config/SecurityConfig.java
+++ src/main/java/com/cczora/armybuilder/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.cczora.armybuilder.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,8 +16,12 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    final UserService userDetails;
+
     @Autowired
-    UserService userDetails;
+    public SecurityConfig(@Lazy UserService userDetails) {
+        this.userDetails = userDetails;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
